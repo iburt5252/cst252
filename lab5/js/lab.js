313d057fbae954037911d707ba8797bf223040bf
iburt5252 <iburton@csumb.edu>
2021-02-15 02:31:18 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Author Ish B//
+//Created 2-14-2021//
+//License: Public Domain//
+
+
+//Defining Variables//
+make = "GMC";
+model = "Yukon";
+color = "Brown";
+year = "2018";
+
+// calculation
+age = 2021 - year;
+
+//output
+document.write("Make: " + make + "<br>");
+document.write("Model: " + model + "<br>");
+document.write("Color: " + color + "<br>");
+document.write("Year: " + year + "<br>");
+document.write("Age: " + age + " years<br>");
