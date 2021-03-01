5b8287a1d23b15dd07bb7b71e7884f9d314ae8f6
iburt5252 <iburton@csumb.edu>
2021-02-28 21:58:14 -0800

    updated 8 number 2

--- lab8/js/lab8.js
+++ lab8/js/lab8.js
@@ -16,10 +16,10 @@ console.log("My array", array);
 
 var restults = array.map(isEven);
 // should return [true, fasle, true, true, true, false, false]
-consle.log("Test of evenness of array:", result);
+console.log("Test of evenness of array:", result);
 
 var resylt = array.map(function(x){
    return x ** 0.5;
 })
 // Should return [10, 9.219544457292887, 3.7416573867739413, 7.483314773547883, 7.211102550927978, 1.7320508075688772, 3]
-consle.log("Squareroot of array:", result);
+console.log("Squareroot of array:", result);
