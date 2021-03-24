170beaa2e558d43996edeec7d162fb6ddb6c0a0b
iburt5252 <iburton@csumb.edu>
2021-03-23 18:50:34 -0700

    lab 14 tues

---
+++ lab14/js/lab.js
@@ -0,0 +1,30 @@
+// Author Ish Burton//
+//Created 3-23-2021//
+//License: Public Domain//
+
+function Vehicle(make, model, year, color, extras, name){
+  this.make = make;
+  this.year = year;
+  this.color = color;
+  this.model = model;
+  this.extras = extras;
+  this.name = name;
+  this.info = function() {
+    return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras; "called" + this.name + " "
+  }
+}
+
+var vehicles = [];
+
+vehicles.push(new Vehicle("Can-am"," DS " , 2021, "orange and black", "LED Lights", "ATV"));
+vehicles.push(new Vehicle("GMC"," Yukon " , 2016, "choclate", "seat warmers", "Big Body Whip"));
+vehicles.push(new Vehicle("GMC"," Sierra " , 2014, "silver", "bed cover", "Truck Truck"));
+
+outputEl = document.getElementById("output")
+for (var i = 0; i<vehicles.length; i++) {
+  var newEl = document.createElement("p");
+  newEl.innerText = vehicles[i].info();
+  outputEl.appendChild(newEl);
+
+
+}
