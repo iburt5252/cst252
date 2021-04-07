e63489b28666fa588bfb3e80b0dc56d458504b2d
iburt5252 <iburton@csumb.edu>
2021-04-06 19:26:39 -0700

    push dat for 16

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -2,14 +2,15 @@
 //Created 4-6-2021//
 //License: Public Domain//
 
+// Append my button, create the button, and give the button an id so I can style it
 $("#outputs").append("<button id='my-button'>Click Me!!!</button>")
 
-
+// Click event listener with attached alert
  $("#my-button").click(function(){
    alert("Made you click! Hahah");
  })
 
-
+ // css for the button, turn it green and change font, padding, border and more
  $("#my-button").css({
 
    "background-color": "green",
