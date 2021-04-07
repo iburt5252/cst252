a0d03159fb7f9049a158f23e965d9c570ab55607
iburt5252 <iburton@csumb.edu>
2021-04-06 19:18:50 -0700

    lab16 done

---
+++ lab16/js/lab.js
@@ -0,0 +1,20 @@
+// Author Ish Burton//
+//Created 4-6-2021//
+//License: Public Domain//
+
+$("#outputs").append("<button id='my-button'>Click Me!!!</button>")
+
+
+ $("#my-button").click(function(){
+   alert("Made you click! Hahah");
+ })
+
+
+ $("#my-button").css({
+
+   "background-color": "green",
+   "font-size": "35pt",
+   "padding": "10px",
+   "color": "white",
+   "font-family": "Fantasy, Copperplate"
+})
