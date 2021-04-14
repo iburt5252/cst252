bf2fbca49e89aec73fec03f63f9e33712eae74fd
iburt5252 <iburton@csumb.edu>
2021-04-13 21:28:05 -0700

    Lab 15 & Lab 17

---
+++ lab15/js/lab.js
@@ -0,0 +1,55 @@
+//Author Ish B/Nick R//
+//created 4-13-2021//
+//Liscense: Public Domian//
+
+
+
+//class Vehicle{
+  //constructor(make, model, year, color, extras, name){
+    //this.make = make;
+    //this.year = year;
+    //this.color = color;
+    //this.model = model;
+    //this.extras = extras;
+    //this.name = name;
+//}
+//info(){
+    //return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras + " called " + this.name ;
+//}
+
+  //}
+class Vehicle {
+  constructor(make, model, year, color, extras, name) {
+
+  this.make = make;
+  this.year = year;
+  this.color = color;
+  this.model = model;
+  this.extras = extras;
+  this.name = name;
+}
+info(){
+  return this.color + " " + this.year + " " + this.make + " " + this.model + " with " + this.extras + " called " + this.name ;
+}
+}
+
+
+var vehicles = [];
+
+vehicles.push(new Vehicle("Custom", "dirt jumper", 2019, "Grey and red", "hand grenade valve caps " , "the craigslist whip"));
+vehicles.push(new Vehicle("Ford", "Explorer", 2004, "Gold", "a missing cigarette lighter and no heater " , "My first car, The Explorer"));
+vehicles.push(new Vehicle("Buick", "Verano", 2016, "Black", "hella trunk space " , "The Buick"));
+vehicles.push(new Vehicle("Enjoi", "Make Jerry Rich pro model", 2011, "All green with a cash bag graphic ", "green venture trucks " , "my board"));
+vehicles.push(new Vehicle("Flip", "Black Logo", 2006, "Red", "too small of trucks " , "my first board"));
+vehicles.push(new Vehicle("RIDE", "Manic", 2014, "Red, white and blue", "one ghetto binding buckle " , "my snowboard"));
+vehicles.push(new Vehicle("Envy", "AOS scooter", 2008, "Black ", "no brake " , "the craiglist scooter"));
+vehicles.push(new Vehicle("FELT", "Scotty Cranmer ", 2010, "Grey and red ", " odysessy doublespace handlebars and thunderbolt cranks, and a shadow stem " , "my first bmx"));
+
+var outputEl = document.getElementById("output");
+
+for (var i = 0; i<vehicles.length; i++) {
+  var newEl = document.createElement("li");
+  newEl.innerText = vehicles[i].info();
+  outputEl.appendChild(newEl);
+
+}
---
+++ lab17/js/lab.js
@@ -0,0 +1,39 @@
+
+// Author Ish B/Nick R//
+//Created 4-13-2021//
+//License: Public Domain//
+
+
+
+
+var myButton = $("<button>");
+
+
+myButton.html("Click Here");
+
+$("#outputs").append(myButton);
+
+myButton.click(function(){
+  myButton.css("background-color", "green");
+  myButton.addClass("green-button");
+  alert("Do not Click");
+
+})
+
+$("#challenge-button").click(function(){
+	$("#challenge-button").css("background-color", "red");
+
+
+});
+
+$("#problems-button").click(function(){
+	$("#problems-button").css("background-color", "blue");
+
+
+});
+
+$("#results-button").click(function(){
+	$("#results-button").css("background-color", "yellow");
+
+
+});
